a232988b53fcca92b40e424b4d60612e9ecc6544
maacaro <maacaro@gmail.com>
2018-07-17 16:42:35 -0400

    Create script to run the server

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const express = require('express')
-const path = require('path')
+let server = require('./server');
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
